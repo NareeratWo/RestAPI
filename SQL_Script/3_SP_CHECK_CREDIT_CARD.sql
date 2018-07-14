23297bb67e9b56cb1fd65551e974cab6ef1ce300
Nareerat Wo <nareerat.wo@kbtg.tech>
2018-07-14 20:59:49 +0700

    update sql create store procedure

--- SQL_Script/3_SP_CHECK_CREDIT_CARD.sql
+++ SQL_Script/3_SP_CHECK_CREDIT_CARD.sql
@@ -8,16 +8,17 @@ GO
 -- Description:	for check credit card 
 -- =============================================
 CREATE PROCEDURE [dbo].[SP_CHECK_CREDIT_CARD]
-@CreditCard_NO integer,
-@expire_date Varchar(6),
-@card_type Varchar(20)
+@Card_NO varchar (16),
+@expire_date nchar(6),
+@card_type nchar(20)
+
 AS
 BEGIN
 	SELECT [CARD_NO]
 		,[EXPIRE_DATE]
 		,[CARD_TYPE]
-	FROM [dbo].[CREDIT_CARD] 
-	WHERE [CARD_NO] = @CreditCard_NO AND [EXPIRE_DATE] = @expire_date AND [CARD_TYPE]=@card_type;
+	FROM [dbo].[CreditCardItem] 
+	WHERE [CARD_NO] = @Card_NO AND [EXPIRE_DATE] = @expire_date AND [CARD_TYPE]=@card_type;
 	
 END
 GO
